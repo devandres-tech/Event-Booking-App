a62d16955bb60d8302810b94e6ba68811e404fe2
andres alcocer <andresalcocer7@yahoo.com>
2020-01-12 23:08:59 -0500

    installed graphql and graphql-express packages

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql')
 
 const app = express();
 
 app.use(bodyParser.json());
+
+// Graphql config
+app.use('/graphql', graphqlHttp({
+  schema: buildSchema(`
+    type RootQuery {
+
+    }
+
+    type RootMutation {
+
+    }
+
+    schema {
+      query: RootQuery
+      mutation: RootMutation
+    }
+  `),
+  rootValue: {}
+}));
 
 app.listen(3000, () => {
   console.log('Server listening on port 3000');

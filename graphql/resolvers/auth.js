f050a32bdd64e17e02f035ab10e55e977ff5d2fc
andres alcocer <andresalcocer7@yahoo.com>
2020-02-11 22:26:41 -0500

    implemented login resolver

--- graphql/resolvers/auth.js
+++ graphql/resolvers/auth.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 // Models
 const User = require('../../models/user');
 
@@ -30,4 +31,24 @@ module.exports = {
         throw err
       })
   },
+  login: async ({ email, password }) => {
+    // validate email, password
+    const user = User.findOne({ email: email });
+    if (!user) {
+      throw new Error('User does not exist');
+    }
+    const isEqual = await bcrypt.compare(password, user.password);
+    if (!isEqual) {
+      throw new Error('Invalid Credentials')
+    }
+    // validate token
+    const token = jwt.sign({ userId: user.id, email: user.email }, 'thesupersecretkey', {
+      expiresIn: '1h'
+    });
+    return {
+      userId: user.id,
+      token: token,
+      tokenExpiration: 1
+    }
+  }
 }
--- graphql/schema/index.js
+++ graphql/schema/index.js
@@ -8,7 +8,6 @@ module.exports = buildSchema(`
       createdAt: String!
       updatedAt: String!
     }
-
     type Event {
       _id: ID!
       title: String!
@@ -37,9 +36,16 @@ module.exports = buildSchema(`
       date: String!
     }
 
+    type AuthData {
+      userId: ID!
+      token: String!
+      tokenExpiration: Int!
+    }
+
     type RootQuery {
       events: [Event!]!
       bookings: [Booking!]!
+      login(email: String!, password: String!): AuthData
     }
 
     type RootMutation {

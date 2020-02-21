3682dcbcfb98a626ddfd5dde914a1734b14c68ec
andres alcocer <andresalcocer7@yahoo.com>
2020-02-20 22:16:50 -0500

    implemented auth form

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react'
 
+import './Auth.css';
+
 class AuthPage extends Component {
   render() {
     return (
-      <div>
-        <h1>The AUthPage page</h1>
-      </div>
+      <form className="auth-form">
+        <div className="form-control">
+          <label htmlFor="email">E-Mail</label>
+          <input type="email" id="email" />
+        </div>
+        <div className="form-control">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" />
+        </div>
+        <div className="form-actions">
+          <button type="submit">Submit</button>
+          <button>Switch to Signup</button>
+        </div>
+      </form>
     )
   }
 }
